fb0ccebcd4632f2901f95a9ff8e010adbcd09607
Rudiansyah <rudiansya24678@gmail.com>
2025-02-16 04:51:14 +0700

    Push project to Github

---
+++ app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import joblib
+
+vectorizer = joblib.load("vectorizer.pkl")
+model = joblib.load("model.pkl")
+
+st.title("Fake News Detector")
+st.write("Enter a News Article to Analyze")
+
+news_input = st.text_area("News Article:","")
+
+if st.button("Check News"):
+    if news_input.strip():
+        transform_input = vectorizer.transform([news_input])
+        prediction = model.predict(transform_input)
+        
+        if prediction[0]==1:
+            st.success("Real News")
+        else:
+            st.error("Fake News")
+            
+    else:
+        st.warning("Please Enter News Article to Analyze!")
